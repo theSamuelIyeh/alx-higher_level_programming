14925a0ebcc02e02c1c3c0aeda5126da8f4955e4
theSamuelIyeh <105016275+theSamuelIyeh@users.noreply.github.com>
2023-01-05 10:42:58 +0100

    Create 8-uppercase.py

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def uppercase(str):
+    newString = ""
+    for i in str:
+      char = ord(i)
+      if 97 <= char <= 122:
+          diff = char - 97
+          newChar = diff + 65
+          newString = newString + chr(newChar)
+      else:
+          newString = newString + i
+      print("{0}".format(newString))
