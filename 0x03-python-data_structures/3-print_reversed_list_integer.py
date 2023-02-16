db2f64fb8fb386cda341fb1b5200925a4bd533f7
Samuel Iyeh <thesamueliyeh@gmail.com>
2023-02-16 13:45:26 +0000

    Correct task 3"

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
+    if my_list == []:
+        return None
     length = len(my_list)
     for i in range(1, length + 1):
         c = i * (-1)
