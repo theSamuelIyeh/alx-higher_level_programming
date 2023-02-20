fae8903a53f266b212427f3ebdd9f3ddb8523072
Samuel Iyeh <thesamueliyeh@gmail.com>
2023-02-20 07:57:28 +0000

    Another commit

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,5 +4,7 @@ def print_matrix_integer(matrix=[[]]):
     for list1 in range(0, len1):
         len2 = len(matrix[list1])
         for list2 in range(0, len2):
-            print("{:d}".format(matrix[list1][list2]), end=" ")
-        print("\n")
+            if list2 == len2 - 1:
+                print("{:d}".format(matrix[list1][list2]))
+            else:
+                print("{:d}".format(matrix[list1][list2]), end=" ")
