868dfcc8dbbb3db48d761e2ff60ab7f1bec26ded
Samuel Iyeh <thesamueliyeh@gmail.com>
2023-02-20 08:56:58 +0000

    Another commit

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    a = 0
-    b = 0
     new_tuple = ()
+    tuple_a_new = tuple_a
+    tuple_b_new = tuple_b
+    if len(tuple_a) < 2:
+        if tuple_a == ():
+            tuple_a_new = (0, 0)
+        else:
+            tuple_a_new = tuple_a + (0,)
+    if len(tuple_b) < 2:
+        if tuple_b == ():
+            tuple_b_new = (0, 0)
+        else:
+            tuple_b_new = tuple_b + (0,)
     for i in range(0, 2):
-        if tuple_a[i]:
-            a = tuple_a[i]
-        if tuple_b[i]:
-            b = tuple_b[i]
+        a = tuple_a_new[i]
+        b = tuple_b_new[i]
         c = a + b
         new_tuple += (c,)
     return new_tuple
