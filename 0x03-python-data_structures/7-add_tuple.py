716759ad4d91e5bbda056e2c796158c0f168e442
Samuel Iyeh <thesamueliyeh@gmail.com>
2023-02-20 08:22:45 +0000

    Another commit

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def add_tuple(tuple_a = (), tuple_b = ()):
+def add_tuple(tuple_a=(), tuple_b=()):
     a = 0
     b = 0
     new_tuple = ()
@@ -9,5 +9,5 @@ def add_tuple(tuple_a = (), tuple_b = ()):
         if tuple_b[i]:
             b = tuple_b[i]
         c = a + b
-        new_tuple += c
+        new_tuple += (c,)
     return new_tuple
