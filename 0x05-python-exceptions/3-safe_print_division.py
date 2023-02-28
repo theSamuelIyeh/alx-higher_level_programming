53a6e697d5525f9a887c66335db348e4bd21b8e3
Samuel Iyeh <thesamueliyeh@gmail.com>
2023-02-28 14:22:25 +0000

    Another commit

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        result = None
+    finally:
+        print("Inside result: {:f}".format(result))
+        return result
