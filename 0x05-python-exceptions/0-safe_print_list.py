d263332a9e1be215da44a0c78a1adb8baae8096b
Samuel Iyeh <thesamueliyeh@gmail.com>
2023-02-28 09:51:06 +0000

    Another commit

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    def safe_print_list(my_list=[], x=0):
+        try:
+            count = 0
+            for i in range(0, x):
+                print("{:d}".format(my_list[i]), end='')
+                count += 1
+        except Exception:
+            return count
