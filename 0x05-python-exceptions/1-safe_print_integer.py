3a7232f63228699c7495a6f12f0e59245a20ba41
Samuel Iyeh <thesamueliyeh@gmail.com>
2023-02-28 10:23:24 +0000

    Another commit

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        print("{:d}".format(value))
+        print("{:d}".format(value), end="")
         print()
         return True
-    except ValueError:
-        print()
+    except (TypeError, ValueError):
         return False
