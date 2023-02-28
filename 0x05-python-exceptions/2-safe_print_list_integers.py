1767e40c6d6855cb952af2a44eef7649f34282a4
Samuel Iyeh <thesamueliyeh@gmail.com>
2023-02-28 10:30:48 +0000

    Another commit

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    try:
+        count = 0
+        for i in range(0, x):
+            print("{:d}".format(my_list[i]), end='')
+            count += 1
+        return count
+    except (ValueError, TypeError):
+        continue
+        pass
+    except IndexError:
+        return count
