e12968715f026ad1b9d8ab1c59e32267343c468d
Samuel Iyeh <thesamueliyeh@gmail.com>
2023-02-28 19:23:38 +0000

    Another commit

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_function(fct, *args):
+    try:
+        res = fct(*args)
+        return res
+    except (IndexError, TypeError) as se:
+        print("Exception: {}".format(se), file=sys.stderr)
+        return None
